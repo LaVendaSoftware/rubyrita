630a7c4ba4498afa0711827e77d3f9e842885f22
Walmir <owalmirneto@gmail.com>
2025-05-24 17:05:27 -0300

    chore: add Combobox component

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,7 @@ pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@hotwired/strada", to: "@hotwired--strada.js" # @1.0.0
+pin "@floating-ui/dom", to: "@floating-ui--dom.js" # @1.7.0
+pin "@floating-ui/core", to: "@floating-ui--core.js" # @1.7.0
+pin "@floating-ui/utils", to: "@floating-ui--utils.js" # @0.2.9
+pin "@floating-ui/utils/dom", to: "@floating-ui--utils--dom.js" # @0.2.9
