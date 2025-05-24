d419d08e38f557c274ce236b4048b8cd36754334
Walmir <owalmirneto@gmail.com>
2025-05-24 10:47:00 -0300

    chore: create platform_identifier method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,17 @@
 module ApplicationHelper
   DEFAULT_PAGE_TITLE = "Rubyrita 🍻"
+  DEFAULT_PLATFORM = "web"
 
   def page_title
     content_for(:turbo_native_title).presence ||
       content_for(:page_title).presence ||
       content_for(:title).presence ||
       DEFAULT_PAGE_TITLE
+  end
+
+  def platform_identifier
+    return "turbo-native" if turbo_native_app?
+
+    DEFAULT_PLATFORM
   end
 end
