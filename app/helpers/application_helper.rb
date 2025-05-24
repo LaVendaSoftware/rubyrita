b6daa90fdae2b9c1128dee3c3f415881f3b43f35
Walmir <owalmirneto@gmail.com>
2025-05-24 10:19:20 -0300

    feat: create page_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  DEFAULT_PAGE_TITLE = "Rubyrita 🍻"
+
+  def page_title
+    content_for(:turbo_native_title).presence ||
+      content_for(:page_title).presence ||
+      content_for(:title).presence ||
+      DEFAULT_PAGE_TITLE
+  end
 end
