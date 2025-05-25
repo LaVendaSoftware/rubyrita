b79898c308f58da1681e5e0b909aabf274fb6443
Walmir <owalmirneto@gmail.com>
2025-05-25 07:56:44 -0300

    test: fix biome offenses

--- app/javascript/controllers/ruby_ui/combobox_controller.js
+++ app/javascript/controllers/ruby_ui/combobox_controller.js
@@ -1,11 +1,11 @@
+import { autoUpdate, computePosition, flip, offset } from "@floating-ui/dom";
 import { Controller } from "@hotwired/stimulus";
-import { computePosition, autoUpdate, offset, flip } from "@floating-ui/dom";
 
 // Connects to data-controller="ruby-ui--combobox"
 export default class extends Controller {
   static values = {
-    term: String
-  }
+    term: String,
+  };
 
   static targets = [
     "input",
@@ -15,151 +15,168 @@ export default class extends Controller {
     "emptyState",
     "searchInput",
     "trigger",
-    "triggerContent"
-  ]
+    "triggerContent",
+  ];
 
-  selectedItemIndex = null
+  selectedItemIndex = null;
 
   connect() {
-    this.updateTriggerContent()
+    this.updateTriggerContent();
   }
 
   disconnect() {
-    if (this.cleanup) { this.cleanup() }
+    if (this.cleanup) {
+      this.cleanup();
+    }
   }
 
   inputChanged(e) {
-    this.updateTriggerContent()
+    this.updateTriggerContent();
 
-    if (e.target.type == "radio") {
-      this.closePopover()
+    if (e.target.type === "radio") {
+      this.closePopover();
     }
 
     if (this.hasToggleAllTarget && !e.target.checked) {
-      this.toggleAllTarget.checked = false
+      this.toggleAllTarget.checked = false;
     }
   }
 
   inputContent(input) {
-    return input.dataset.text || input.parentElement.textContent
+    return input.dataset.text || input.parentElement.textContent;
   }
 
   toggleAllItems() {
-    const isChecked = this.toggleAllTarget.checked
-    this.inputTargets.forEach(input => input.checked = isChecked)
-    this.updateTriggerContent()
+    const isChecked = this.toggleAllTarget.checked;
+    // biome-ignore lint/complexity/noForEach: <explanation>
+    // biome-ignore lint/suspicious/noAssignInExpressions: <explanation>
+    this.inputTargets.forEach((input) => (input.checked = isChecked));
+    this.updateTriggerContent();
   }
 
   updateTriggerContent() {
-    const checkedInputs = this.inputTargets.filter(input => input.checked)
+    const checkedInputs = this.inputTargets.filter((input) => input.checked);
 
-    if (checkedInputs.length == 0) {
-      this.triggerContentTarget.innerText = this.triggerTarget.dataset.placeholder
+    if (checkedInputs.length === 0) {
+      this.triggerContentTarget.innerText =
+        this.triggerTarget.dataset.placeholder;
     } else if (checkedInputs.length === 1) {
-      this.triggerContentTarget.innerText = this.inputContent(checkedInputs[0])
+      this.triggerContentTarget.innerText = this.inputContent(checkedInputs[0]);
     } else {
-      this.triggerContentTarget.innerText = `${checkedInputs.length} ${this.termValue}`
+      this.triggerContentTarget.innerText = `${checkedInputs.length} ${this.termValue}`;
     }
   }
 
   openPopover(event) {
-    event.preventDefault()
+    event.preventDefault();
 
-    this.updatePopoverPosition()
-    this.updatePopoverWidth()
-    this.triggerTarget.ariaExpanded = "true"
-    this.selectedItemIndex = null
-    this.itemTargets.forEach(item => item.ariaCurrent = "false")
-    this.popoverTarget.showPopover()
+    this.updatePopoverPosition();
+    this.updatePopoverWidth();
+    this.triggerTarget.ariaExpanded = "true";
+    this.selectedItemIndex = null;
+    // biome-ignore lint/complexity/noForEach: <explanation>
+    this.itemTargets.forEach((item) => {
+      item.ariaCurrent = "false";
+    });
+    this.popoverTarget.showPopover();
   }
 
   closePopover() {
-    this.triggerTarget.ariaExpanded = "false"
-    this.popoverTarget.hidePopover()
+    this.triggerTarget.ariaExpanded = "false";
+    this.popoverTarget.hidePopover();
   }
 
   filterItems(e) {
     if (["ArrowDown", "ArrowUp", "Tab", "Enter"].includes(e.key)) {
-      return
+      return;
     }
 
-    const filterTerm = this.searchInputTarget.value.toLowerCase()
+    const filterTerm = this.searchInputTarget.value.toLowerCase();
 
     if (this.hasToggleAllTarget) {
-      if (filterTerm) this.toggleAllTarget.parentElement.classList.add("hidden")
-      else this.toggleAllTarget.parentElement.classList.remove("hidden")
+      if (filterTerm)
+        this.toggleAllTarget.parentElement.classList.add("hidden");
+      else this.toggleAllTarget.parentElement.classList.remove("hidden");
     }
 
-    let resultCount = 0
+    let resultCount = 0;
 
-    this.selectedItemIndex = null
+    this.selectedItemIndex = null;
 
+    // biome-ignore lint/complexity/noForEach: <explanation>
     this.inputTargets.forEach((input) => {
-      const text = this.inputContent(input).toLowerCase()
+      const text = this.inputContent(input).toLowerCase();
 
       if (text.indexOf(filterTerm) > -1) {
-        input.parentElement.classList.remove("hidden")
-        resultCount++
+        input.parentElement.classList.remove("hidden");
+        resultCount++;
       } else {
-        input.parentElement.classList.add("hidden")
+        input.parentElement.classList.add("hidden");
       }
-    })
+    });
 
-    this.emptyStateTarget.classList.toggle("hidden", resultCount !== 0)
+    this.emptyStateTarget.classList.toggle("hidden", resultCount !== 0);
   }
 
   keyDownPressed() {
     if (this.selectedItemIndex !== null) {
-      this.selectedItemIndex++
+      this.selectedItemIndex++;
     } else {
-      this.selectedItemIndex = 0
+      this.selectedItemIndex = 0;
     }
 
-    this.focusSelectedInput()
+    this.focusSelectedInput();
   }
 
   keyUpPressed() {
     if (this.selectedItemIndex !== null) {
-      this.selectedItemIndex--
+      this.selectedItemIndex--;
     } else {
-      this.selectedItemIndex = -1
+      this.selectedItemIndex = -1;
     }
 
-    this.focusSelectedInput()
+    this.focusSelectedInput();
   }
 
   focusSelectedInput() {
-    const visibleInputs = this.inputTargets.filter(input => !input.parentElement.classList.contains("hidden"))
+    const visibleInputs = this.inputTargets.filter(
+      (input) => !input.parentElement.classList.contains("hidden"),
+    );
 
-    this.wrapSelectedInputIndex(visibleInputs.length)
+    this.wrapSelectedInputIndex(visibleInputs.length);
 
     visibleInputs.forEach((input, index) => {
-      if (index == this.selectedItemIndex) {
-        input.parentElement.ariaCurrent = "true"
-        input.parentElement.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'nearest' })
+      if (index === this.selectedItemIndex) {
+        input.parentElement.ariaCurrent = "true";
+        input.parentElement.scrollIntoView({
+          behavior: "smooth",
+          block: "nearest",
+          inline: "nearest",
+        });
       } else {
-        input.parentElement.ariaCurrent = "false"
+        input.parentElement.ariaCurrent = "false";
       }
-    })
+    });
   }
 
   keyEnterPressed(event) {
-    event.preventDefault()
-    const option = this.itemTargets.find(item => item.ariaCurrent === "true")
+    event.preventDefault();
+    const option = this.itemTargets.find((item) => item.ariaCurrent === "true");
 
     if (option) {
-      option.click()
+      option.click();
     }
   }
 
   wrapSelectedInputIndex(length) {
-    this.selectedItemIndex = ((this.selectedItemIndex % length) + length) % length
+    this.selectedItemIndex =
+      ((this.selectedItemIndex % length) + length) % length;
   }
 
   updatePopoverPosition() {
     this.cleanup = autoUpdate(this.triggerTarget, this.popoverTarget, () => {
       computePosition(this.triggerTarget, this.popoverTarget, {
-        placement: 'bottom-start',
+        placement: "bottom-start",
         middleware: [offset(4), flip()],
       }).then(({ x, y }) => {
         Object.assign(this.popoverTarget.style, {
@@ -171,6 +188,6 @@ export default class extends Controller {
   }
 
   updatePopoverWidth() {
-    this.popoverTarget.style.width = `${this.triggerTarget.offsetWidth}px`
+    this.popoverTarget.style.width = `${this.triggerTarget.offsetWidth}px`;
   }
 }
