d0317609859f0a3f01c95597141faf11d7a8e10a
Walmir <owalmirneto@gmail.com>
2025-05-27 21:06:53 -0300

    fix: home page and layout

--- app/views/home/index.rb
+++ app/views/home/index.rb
@@ -1,13 +1,14 @@
 class Views::Home::Index < Views::Base
   include Phlex::Rails::Helpers::ImageTag
+  include Phlex::Rails::Helpers::LinkTo
 
   def view_template
     div(class: "flex flex-col") do
       div(class: "flex items-center justify-center") do
         div(class: "flex flex-col justify-center") do
-          h1(class: "font-bold text-4xl") do
-            image_tag("rubyrita.png", class: "h-32")
-          end
+          image_tag("rubyrita.png", class: "h-64 mt-12")
+
+          div(class: "text-center text-3xl mt-4") { link_to("✏️ Artigos", articles_path) }
         end
       end
     end
